000fc1d50e82c983b9e874b79d9472adf33f642a
oleksii_mialo <oleksii.mialo@headworks.com.ua>
2024-05-11 17:19:17 +0300

    initial comit

---
+++ src/Containers/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+import { UserListPage } from './UserListPage/UserListPage';
+
+function App() {
+  return <>
+    <div className="App">
+
+      <UserListPage />
+
+    </div>
+  </>
+}
+
+export default App;
---
+++ src/Containers/UserListPage/UserListPage.jsx
@@ -0,0 +1,10 @@
+import styles from './UserListPage.module.scss'
+
+export function UserListPage() {
+    return <header className={styles['header']}>
+
+        <h1>UserList App</h1>
+        <p>..inspired by ChatGPT test task</p>
+
+    </header>
+}
