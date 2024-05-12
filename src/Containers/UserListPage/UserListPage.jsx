09b5a6b9973754eb6c92bad31d4bfec4ae40802d
oleksii_mialo <oleksii.mialo@headworks.com.ua>
2024-05-12 08:36:35 +0300

    added footer

--- src/Containers/UserListPage/UserListPage.jsx
+++ src/Containers/UserListPage/UserListPage.jsx
@@ -7,10 +7,23 @@ export function UserListPage() {
         <header className={styles['header']}>
 
             <h1>UserList App</h1>
-            <p><i>..inspired by ChatGPT test task</i></p>
+            <p><i>..inspired by ChatGPT test task</i><a href='#description'> *</a></p>
 
         </header>
 
         <UserTable />
+
+        <footer id='description' className={styles['footer']}>
+            <a href='#description'>* </a>
+            <p>I asked Chat to generate test task for junior frontend developer - and my implementation of that here. No other cheats with LLM</p>
+
+            <br /> <br />
+
+            <span>Sure it looks very simple so I hope my code shows more about my possibilities: </span>
+            <a className={styles['github-link']} target='_blank' href='https://github.com/malexit240'>
+                github link
+            </a>
+
+        </footer>
     </>
 }
